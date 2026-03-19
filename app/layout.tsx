e031d5d8f7e578de62f92d814daddaffd19c1a7b
CoolCream <mahasankarshant@gmail.com>
2026-03-19 12:02:23 +0530

    settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -62,6 +63,7 @@ export default function RootLayout({
           >
             <ConvexClientProvider>
               <Toaster position="bottom-center" />
+              <ModalProvider />
               {children}
             </ConvexClientProvider>
           </ThemeProvider>
